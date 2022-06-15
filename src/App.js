d4cc85939425c634bce11940ccaf434393d351a9
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-15 14:46:31 +0530

    Final CSS styling all required styling done

--- src/App.js
+++ src/App.js
@@ -11,13 +11,12 @@ const emojiDictionary = {
   "🙈": "See no evil monkey",
   "❤️": "Love",
   "🔥": "Fire",
+  "🌚": "New moon face",
   "🤠": "Cowboy hat face",
   "🙃": "Upside down face",
   "😑": "Annoyance",
   "😶": "Face without mouth"
 };
-
-var headingColor = "lightblue";
 
 // this function converts given object into array
 var knownEmojis = Object.keys(emojiDictionary);
@@ -44,35 +43,71 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1
-        style={{
-          backgroundColor: headingColor,
-          fontSize: "3rem",
-          color: "#164e63"
-        }}
-      >
-        {" "}
-        Know your Emoji{" "}
-      </h1>
-      <input onChange={emojiInputHandler}></input>
-
-      {/* this is the actual output set by REACT using USESTATE */}
-      <h1 style={{ color: "#581c87" }}> {meaning} </h1>
-
-      <h3 style={{ color: "#fb7185" }}>
-        Click on any Emoji to know it's meaning
-      </h3>
-      {knownEmojis.map(function (emoji) {
-        return (
-          <span
-            onClick={() => emojiClickHandler(emoji)}
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-            key={emoji}
-          >
-            {emoji}
-          </span>
-        );
-      })}
+      <nav className="navigation">
+        <h1 className="nav-head">🌚 Know your Emoji 🌚</h1>
+      </nav>
+      <div className="input-area">
+        <input
+          onChange={emojiInputHandler}
+          placeholder="paste your emoji here"
+        ></input>
+      </div>
+      <div className="output-area">
+        {/* this is the actual output set by REACT using USESTATE */}
+        <h1> {meaning} </h1>
+      </div>
+      <div className="database-area">
+        <h3>Click on any Emoji to know it's meaning</h3>
+        {knownEmojis.map(function (emoji) {
+          return (
+            <span
+              onClick={() => emojiClickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+              key={emoji}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+      <footer className="footer">
+        <h3 className="footer-head">About</h3>
+        <p>
+          <strong>💕 Just a simple Emoji Indetifier 💕</strong>
+        </p>
+        <ul className="list-footer">
+          <li className="list-footer-item">
+            <a
+              className="link"
+              href="https://github.com/aayushrajj"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/github.svg" alt="github" />
+            </a>
+          </li>
+          <li className="list-footer-item">
+            <a
+              className="link"
+              href="https://linkedin.com/in/ayush-raj-0309"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/linkedin.svg" alt="linkedin" />
+            </a>
+          </li>
+          <li className="list-footer-item">
+            <a
+              className="link"
+              href="https://twitter.com/AyushRajgupt"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/twitter.svg" alt="twitter" />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
